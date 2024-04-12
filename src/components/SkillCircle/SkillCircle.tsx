5ad43ec578c1920d057d8cc2d2fc4c671ff39beb
abdullatif <abdullatifjr002@gmail.com>
2024-04-12 23:35:33 +0300

    bug fixes

--- src/components/SkillCircle/SkillCircle.tsx
+++ src/components/SkillCircle/SkillCircle.tsx
@@ -3,27 +3,27 @@ import './SkillCircle.css'
 const SkillCircle = () => {
   return (
     <div className='skills-circles'>
-      <div className='progress' style={{'--i': '90'}}>
+      <div className='progress'>
         <h3>90<span>%</span></h3>
         <h4>Html</h4>
       </div>
-      <div className='progress' style={{'--i': '85'}}>
+      <div className='progress'>
         <h3>85<span>%</span></h3>
         <h4>CSS</h4>
       </div>
-      <div className='progress' style={{'--i': '80'}}>
+      <div className='progress'>
         <h3>80<span>%</span></h3>
         <h4>JS</h4>
       </div>
-      <div className='progress' style={{'--i': '90'}}>
+      <div className='progress'>
         <h3>90<span>%</span></h3>
         <h4>Bootstrap</h4>
       </div>
-      <div className='progress' style={{'--i': '70'}}>
+      <div className='progress'>
         <h3>70<span>%</span></h3>
         <h4>React</h4>
       </div>
-      <div className='progress less' style={{'--i': '30'}}>
+      <div className='progress less'>
         <h3>30<span>%</span></h3>
         <h4>TypeScript</h4>
       </div>
